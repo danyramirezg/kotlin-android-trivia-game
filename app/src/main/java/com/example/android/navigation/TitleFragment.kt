0f9aacde38db69545bed68f1578c2d781d8211cc
danyramirezg <danyramirezg@gmail.com>
2021-05-30 18:57:24 -0500

    Add a menu in TitleFragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -23,9 +22,22 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        // Tell Android that the TitleFragment has a menu:
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    // Inflate the menu resource:
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
 
-
+    // This function is to connect it to the NavigationUI
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // If the navigationUI doesn't handle the selection, so call super on optionsItemSelected
+        // which allows the app to directly handle the menu item without navigating
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
